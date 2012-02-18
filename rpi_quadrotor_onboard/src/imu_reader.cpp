97381f30f7caf95f9d4ce53699a41230e634b30a
William Sisson <sissow2@macbits.troy.drawersteak.com>
2012-02-18 18:02:11 -0500

    made ros work for reals

---
+++ rpi_quadrotor_onboard/src/imu_reader.cpp
@@ -0,0 +1,13 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <iostream>
+
+
+
+int main (int argc, char ** argv) 
+{
+	ros::init(argc, argv, "imu_reader");
+	ros::NodeHandle node;
+	
+	
+}
