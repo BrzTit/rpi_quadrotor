fd82b258ed6c67f026dcf5297ae683e083d9a3be
sissow2 <williamsisson2@gmail.com>
2012-02-19 18:33:06 -0500

    IMU data now works with ROS

--- rpi_quadrotor_onboard/imu_reader/imu_reader.cpp
+++ rpi_quadrotor_onboard/imu_reader/imu_reader.cpp
@@ -2,32 +2,29 @@
 #include "std_msgs/String.h"
 
 #include "driver_sparkfun6dof/sparkfun6dof.h"
+#include "rpi_quadrotor_onboard/imu_data.h"
 
 #include <iostream>
 #include <sstream>
 
-
+using namespace rpi_quadrotor_onboard;
 
 int main (int argc, char ** argv) 
 {
 	ros::init(argc, argv, "imu_reader");
 	ros::NodeHandle node;
 	
-	ros::Publisher imu_publisher = node.advertise<std_msgs::String>("imu_data", 240);
-	
-	//ros::Rate loop_rate(10);
-	
+	ros::Publisher imu_publisher = node.advertise<imu_data>("imu_data", 240);
+
 	Sparkfun6DOF::imudata data;
 	Sparkfun6DOF imu("/dev/tty.usbserial-A7005FSa");
 	
 	while (ros::ok()) {
 		std::stringstream ss;
-		std_msgs::String msg;
+		imu_data msg;
 		
 		imu.getIMUData(data);
-		
-		ss << "hey guys " << data.count;
-		msg.data = ss.str();
+		memcpy(& msg.count, & data.count, 14);
 		
 		imu_publisher.publish(msg);
 		
